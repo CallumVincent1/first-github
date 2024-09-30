9e1f23c216a221b701beed3019e8a563a75ac7a7
Callum Vincent (s5709117) <s5709117@bournemouth.ac.uk>
2024-09-30 16:23:44 +0100

    initial message

---
+++ firstCode.py
@@ -0,0 +1,24 @@
+name = "alex"
+age = 20
+print(f"{name} is {age} years old")
+
+name = "alice"
+print("hello " + name)
+print("{} is {} years old".format("Alice", 30))
+
+a = "hello "
+b = "world"
+result = a+b
+print(result)
+
+
+print(265 + 5924)
+print(5022 - 901)
+print(5*10)
+print(604/4)
+print(3%2)
+print(10%4)
+print(9//3)
+print(8//3)
+print(5**2)
+print(4**3)
